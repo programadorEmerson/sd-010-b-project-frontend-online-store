3f3ac4259b26a57dd5f2ad7376ec070b1e28326c
Yago Rocha de Souza Lima <yagorochasouza@gmail.com>
2021-05-14 15:36:41 -0300

    corrige req1

--- src/services/api.js
+++ src/services/api.js
@@ -5,5 +5,5 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  return fetch(url).then((products) => products.json);
+  return fetch(url).then((products) => products.json());
 }
