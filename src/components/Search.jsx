a6014018ba2ea9753f0bcc7141f8fe7c3886fcc6
edersonrodara <edersonrodara@gmail.com>
2021-05-14 16:29:19 -0300

    requisito 2 concluido

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <form>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <input type="text" />
+      </form>
+    );
+  }
+}
+
+export default Search;
