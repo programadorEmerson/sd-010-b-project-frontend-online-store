dd4c47c8ebe54a567ba36e0a2db87bc98d40da4a
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-14 16:13:12 -0300

    Feito requisito 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <span data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </span>
     </div>
   );
 }
