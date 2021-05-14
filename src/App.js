05788a8de9c4a10c4808cf54989a800212fc0d4d
Emerson Saturnino <programador.emerson@hotmail.com>
2021-05-14 15:30:30 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Vms que vamos
+          Grupo Requisito 1
         </a>
       </header>
     </div>
