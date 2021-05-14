b36bb8436482e71d33d2363689d6068eaf84d84d
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-14 18:26:58 -0300

    adicionando imagem no link do carrinho

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import btnIcon from '../img/iconeCompras.png'
+
+
 class Home extends React.Component {
+  // handleOnClick = (target) => {
+  //   <Link data-testid="shopping-cart-button" to={`/${target.alt}`}></Link>
+  // }
   render() {
     return (
       <div>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-        <Link data-testid="shopping-cart-button" to="/checkout">Carrinho</Link>
+        <Link data-testid="shopping-cart-button" to='/checkout'>
+          <img width='30px' src={btnIcon} alt="checkout"></img>
+        </Link>
       </div>
     );
   }
