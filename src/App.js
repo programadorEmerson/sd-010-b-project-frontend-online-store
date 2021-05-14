8703c2f2a7b3a24380577f0fbfc8d8bb9de8a1fa
Thiago da Paz <thiagopazjunior@gmail.com>
2021-05-14 18:44:33 -0300

    Add req5

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import ShoppingCart from './components/ShoppingCart';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <div>
-        <label htmlFor="search" data-testid="home-initial-message">
-          <input type="text" name="search" value="search-text" />
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      searchText: '',
+    }
+  }
 
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </label>
-      </div>
-      <BrowserRouter>
+  render() {
+    setSearchText({ target }) => {
+      this.setState({
+        searchText: target.value,
+      })
+    }
+
+    return (
+      <div className="App">
         <div>
-          <Link to="/shopping-cart" data-testid="shopping-cart-button" />
+          <label htmlFor="search" data-testid="home-initial-message">
+            <input type="text" name="search" onChange="" />
+
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </label>
         </div>
-        <Switch>
-          <Route exact path="/" render={ () => <ProductList /> } />
-          <Route path="/shopping-cart" render={ () => <ShoppingCart /> } />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+        <BrowserRouter>
+          <div>
+            <Link to="/shopping-cart" data-testid="shopping-cart-button" />
+          </div>
+          <Switch>
+            <Route exact path="/" render={ () => <ProductList /> } />
+            <Route path="/shopping-cart" render={ () => <ShoppingCart /> } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
