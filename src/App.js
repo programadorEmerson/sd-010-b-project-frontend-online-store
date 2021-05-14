1624c0e9784ecd706a17998f335318013af0c41e
Marconi <marconieng@gmail.com>
2021-05-14 17:21:37 -0300

    Adiciona requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import ProductsList from './Components/ProductsList';
 // import * as api from './services/api';
 
 function App() {
   // console.log(api.getCategories());
   return (
-    <div className="App" />
+    <main>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={ ProductsList } />
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
