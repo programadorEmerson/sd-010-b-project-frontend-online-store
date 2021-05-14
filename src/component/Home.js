cc45a7a6d54ab43163ffb7d48e14ae666da82fc2
Pedrodev1 <pedro.oliveira.dev@outlook.com>
2021-05-14 17:39:59 -0300

    Req 5

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,54 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listProduct: [],
+      search: '',
+    };
+    this.getResult = this.getResult.bind(this);
+    this.requestApi = this.requestApi.bind(this);
+    this.resultMap = this.resultMap.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestApi();
+  }
+
+  getResult(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+
+  async requestApi() {
+    const result = await getCategories();
+    this.setState({
+      listProduct: result,
+    });
+  }
+
+  resultMap() {
+    const { listProduct } = this.state;
+    const product = listProduct.map((products) => (
+      <li key={ products.id }>{products.name}</li>
+    ));
+    return product;
+  }
+
   render() {
+    // const { listProduct } = this.state;
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <ol>
+          {this.resultMap()}
+        </ol>
+      </div>
     );
   }
 }
