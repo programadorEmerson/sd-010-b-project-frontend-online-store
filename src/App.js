0d0cea58ae7c9ed212d0c32a991f48febf4c5311
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-05-14 18:43:53 -0300

    Importando a página de carrinho de compras e a sua rota

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Search from './pages/Search';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Switch>
           <Route exact path="/" component={ Search } />
+          <Route exact path="/cart" component={ Cart } />
         </Switch>
       </main>
     </BrowserRouter>
