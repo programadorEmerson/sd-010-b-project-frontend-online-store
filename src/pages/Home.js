4f9dd7d9fbc26067111fe3d5dd97ee22db48d3e3
Luiz Reina <luizreeina@gmail.com>
2021-05-14 16:47:34 -0300

    resolving conflicts between req2 and req3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,14 @@ class Home extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Front end online store grupo 4</h1>
+        <div>
+          <input type="text" />
+          <p
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </div>
         <Link data-testid="shopping-cart-button" to="/cart">VER CARRINHO</Link>
       </div>
     );
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
   // Implemente aqui
+  const returnCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const dataCat = await returnCategories.json();
+  return dataCat;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const returnProducts = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const dataProd = await returnProducts.json();
+  return dataProd;
 }
