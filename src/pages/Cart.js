3dba0242d8002b4b31c6bf7c50a24adea5e7751b
Luiz Reina <luizreeina@gmail.com>
2021-05-14 16:35:41 -0300

    requirement 3, cart button at homepage and redirect to /cart - codeClimate

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Cart extends React.Component {
   render() {
     return (
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
-      );
+      <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+    );
   }
 }
 
