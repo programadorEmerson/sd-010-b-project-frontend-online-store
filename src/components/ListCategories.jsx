dca46bea3916b33c29a250e7b06867cff8fb5eaf
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-05-14 19:26:34 -0300

    criando estado inicial

---
+++ src/components/ListCategories.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ListCategories extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      listCategories: [],
+    };
+  }
+
+  render() {
+    return (
+      <aside />
+    );
+  }
+}
+
+export default ListCategories;
