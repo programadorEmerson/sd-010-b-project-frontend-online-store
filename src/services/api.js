0a666ae3cab66982683b6d6a005f936404f52c44
Jonathan Souza <souzajonathan570@gmail.com>
2021-05-14 19:13:34 -0300

    correçao da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,17 @@
 export async function getCategories() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const result = await response.json();
-  return result;
+  // const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  // const result = await response.json();
+  // return result;
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories%27')
+    .then((response) => response.json())
+    .then((data) => data);
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
-  const result = await response.json();
-  return result;
+  // const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+  // const result = await response.json();
+  // return result;
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
+    .then((response) => response.json())
+    .then((data) => data);
 }
