fa08bee555b5226141c4b74072b80ad72d52f1c5
Aderson Santos <assaderson@gmail.com>
2021-05-14 16:37:51 -0300

    Primeiro requisito sendo montado pela equipe 12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { getCategories } from './services/api';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount = () => {
+    this.saveState();
+  }
+
+  saveState = async () => {
+    const listCategories = await getCategories();
+    this.setState({ categories: listCategories },
+      () => {
+        console.log(this.state);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Frontend Online Store </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 export async function getCategories() {
-  // Implemente aqui
+  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories', { method: 'GET' });
+  const responseCategories = await categories.json();
+  return responseCategories;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+// nada
 }
