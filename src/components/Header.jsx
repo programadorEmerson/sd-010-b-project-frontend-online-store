6ae02e6f641cd4ab6a7fc4acafd43bf751dbe322
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-05-14 18:36:35 -0300

    requisito 3

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+      <form>
+        <button type="submit">Carrinho de compras</button>
+          <input type="text">
+            Meu carrinho está vazio
+          </input>
+
+      </form>)
+  }
+}
+
+export default Header;
