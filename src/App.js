e3c0844cefe3e28800fe1a25bb4f01876d9bde44
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-05-14 17:29:40 -0300

    Feat: requirement 2 - DONE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-//comentario teste
+// import api from './services/api';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <input type="text" />
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    </section>
   );
 }
 
 export default App;
-
-// Teste PR branch principal
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetchCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await fetchCategories.json();
+  return categories;
+}
+console.log(getCategories());
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchCategoriesQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categoryQuery = await fetchCategoriesQuery.json();
+  return categoryQuery;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-}
+console.log(getProductsFromCategoryAndQuery('MLB1384', 'Cadeira'));
