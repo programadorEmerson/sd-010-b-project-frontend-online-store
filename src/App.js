9cecbbd526ac7a67628f36855f61262698d20e80
Matheus H. Gois <matheus.hengo@gmail.com>
2021-05-14 17:09:16 -0300

    Route to shopping cart page and link button created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import ShoppingCart from './pages/ShoppingCart';
 // import * as api from './services/api';
 
 function App() {
   // console.log(api.getCategories());
   return (
-    <div className="App" />
+    <div className="App">
+      <BrowserRouter>
+        <Link to="/cart">Cart</Link>
+
+        <Switch>
+          <Route path="/cart" component={ ShoppingCart } />
+        </Switch>
+
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/pages/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Carrinho de Compras</h1>
+        <p>Carrinho Vazio!</p>
+      </section>
+    );
+  }
+}
+
+export default ShoppingCart;
