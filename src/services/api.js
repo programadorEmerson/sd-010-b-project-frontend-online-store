27da3c6cd0804065fd02252e83cb000c8c22c8c8
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-05-14 16:21:53 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
   // Implemente aqui
+  const URL = "https://api.mercadolibre.com/sites/MLB/categories";
+  return fetch(URL).then((result) => result.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  return fetch(URL).then((result) => result.json());
 }
