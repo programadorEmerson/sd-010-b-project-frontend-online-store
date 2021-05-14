058ec63844f3c8f65c5551a84fb2b5701836c6f1
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-05-14 19:04:16 -0300

    Feito requisito 4

--- src/Components/ShoppingCart.js
+++ src/Components/ShoppingCart.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as api from '../services/api';
 
 class ShoppingCart extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lista: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((result) => {
+      this.setState({ lista: result });
+    });
+  }
+
+  renderElement = () => {
+    const { lista } = this.state;
+    return lista.map((element) => (
+      <li key={ element.id } data-testid="category">
+        { element.name }
+      </li>
+    ));
+  }
+
   render() {
     return (
       <div>
@@ -9,6 +33,7 @@ class ShoppingCart extends React.Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
         <Link data-testid="shopping-cart-button" to="/checkout">Compra</Link>
+        { this.renderElement() }
       </div>
     );
   }
