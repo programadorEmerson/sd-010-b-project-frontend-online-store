51d19ef1325a5093bd261164f110e7c6430abf1e
Caio <thehaseol@gmail.com>
2021-05-14 17:31:28 -0300

    resoluçao de erros de Lint

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import * as Api from '../services/api';
+import CategoryList from './CategoryList';
 
 class Homepage extends React.Component {
   constructor() {
     super();
     this.state = {
       searchQuery: '',
+      categories: [],
     };
+
+    this.LoadCategories = this.LoadCategories.bind(this);
+    this.renderCategories = this.renderCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.LoadCategories();
+  }
+
+  async LoadCategories() {
+    const allCategories = await Api.getCategories();
+    this.setState({
+      categories: allCategories,
+    });
+  }
+
+  renderCategories() {
+    const { categories } = this.state;
+    return (
+      <section className="category-list">
+        {categories.length === 0 ? (
+          <span>Nenhuma categoria foi encontrada</span>
+        ) : (
+          categories.map((categ) => (
+            <CategoryList key={ categ.id } name={ categ.name } />
+          ))
+        )}
+      </section>
+    );
   }
 
   render() {
@@ -19,6 +51,7 @@ class Homepage extends React.Component {
         />
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
+          {this.renderCategories()}
         </h3>
       </div>
     );
