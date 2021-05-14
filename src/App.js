de293f65585ddf50d57679afc91954660f7f08dd
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-05-14 16:18:12 -0300

    Implementando funçao getCategories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import * as api from './services/api';
 import './App.css';
 
 function App() {
+  (api.getCategories().then((res) => console.log(res)));
+
   return (
     <div className="App">
       <header className="App-header">
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,10 @@ export async function getCategories() {
   const categories = await fetch(
     'https://api.mercadolibre.com/sites/MLB/categories',
   );
-  return categories;
+
+  const response = categories.json();
+
+  return response;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
