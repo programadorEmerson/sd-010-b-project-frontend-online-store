4a987fd1219609519122c422504ede2b003654ad
Alan Tanaka <alanctnk@gmail.com>
2021-05-14 17:38:07 -0300

    Requisito 2 finalizado

--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,12 @@ export async function getCategories() {
   const response = await fetch(endPoint);
   const categories = response.json();
   return categories;
-  // Implemente aqui
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const response = await fetch(endPoint);
   const productsByCategory = response.json();
   return productsByCategory;
 }
+
