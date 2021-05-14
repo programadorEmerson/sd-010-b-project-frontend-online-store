c580cd2411e96229483779814c5f17022b95b865
Thiago da Paz <thiagopazjunior@gmail.com>
2021-05-14 16:42:04 -0300

    Corrigindo lint.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'; 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
       <label htmlFor="search" data-testid="home-initial-message">
-        <input type="text" name="search" value="search-text"/>
+        <input type="text" name="search" value="search-text" />
+
         Digite algum termo de pesquisa ou escolha uma categoria.
-      </label>  
+      </label>
       <BrowserRouter>
         <Switch>
-            <Route path="/" render={() => <ProductList />} />
-        </Switch> 
+          <Route path="/" render={ () => <ProductList /> } />
+        </Switch>
       </BrowserRouter>
     </div>
   );
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class ProductList extends React.Component {
   render() {
-    return(
+    return (
       true
     );
   }
