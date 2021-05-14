82edd9b693a9d5f8695044416444c4983ec344cc
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-14 17:58:46 -0300

    adicionando novo componente Checkout e o botao(link) na pagina home

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Checkout extends React.Component {
+  render() {
+    return (
+      <p data-testid="shopping-cart-empty-message">
+        Seu carrinho está vazio      
+      </p>
+    );
+  }
+}
+
+export default Checkout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Checkout from './Checkout';
 
 class Home extends React.Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link data-testid="shopping-cart-button" to="/checkout">Carrinho</Link>
+        <Route path="/checkout" component={ Checkout } />
+      </div>
     );
   }
 }
