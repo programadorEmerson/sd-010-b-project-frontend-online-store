5742ffd8ddc5429acb1926e07c2270c1af56e6e0
riquee <strikedv@hotmail.com>
2021-05-14 20:28:12 -0300

    create ListCategory

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import { Route, Switch, Link, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import ShoppingCart from './components/ShoppingCart';
+import { getCategories } from './services/api'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      category: [],
       cart: [],
     };
+
+    this.updateCategory = this.updateCategory.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateCategory();
+  }
+
+  async updateCategory() {
+    const response = await getCategories();
+    this.setState({ category: response });
   }
 
   render() {
@@ -18,7 +31,7 @@ class App extends Component {
       <BrowserRouter>
         <Header />
         <p>
-          <Link data-testid="shopping-cart-button" to="/cart">Oi</Link>
+          <Link data-testid="shopping-cart-button" to="/cart">Oii</Link>
         </p>
         <Switch>
           <Route path="/cart" render={ () => <ShoppingCart cart={ cart } /> } />
