ddacda1b923d4aae56eaf9e4738baa4702087250
aythan azevedo <aythan16@gmail.com>
2021-05-14 18:16:45 -0300

    button cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Cart from './pages/Cart';
 import Mensagem from './components/Mensagem';
+import CartButton from './components/CartButton';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" component={ Mensagem } />
+        <Switch>
+          <Route path="/cart" component={ Cart } />
+          <Route exact path="/" component={ Mensagem } />
+        </Switch>
+        <CartButton />
       </BrowserRouter>
     </div>
   );
