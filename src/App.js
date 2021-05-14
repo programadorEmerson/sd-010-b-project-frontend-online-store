b0dbf3aced990a932371c7d00fa203806f17cea6
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-14 15:48:56 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
     </div>
   );
 }
