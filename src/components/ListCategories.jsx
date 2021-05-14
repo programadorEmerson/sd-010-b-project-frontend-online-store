697049f464cee848e24c98fdfde2095174adccef
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-05-14 19:43:43 -0300

    criando funcao que altera estado do componente

--- src/components/ListCategories.jsx
+++ src/components/ListCategories.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { getCategories } from '../services/api';
 
 class ListCategories extends Component {
   constructor() {
@@ -7,11 +8,27 @@ class ListCategories extends Component {
     this.state = {
       listCategories: [],
     };
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  async fetchCategories() {
+    this.setState({
+      listCategories: await getCategories(),
+    });
   }
 
   render() {
     return (
-      <aside />
+      <aside>
+        <nav>
+          <ul />
+        </nav>
+      </aside>
     );
   }
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ListCategories from '../ListCategories';
 
 export default class Home extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class Home extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <ListCategories />
       </div>
     );
   }
