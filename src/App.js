781f1f9fe4888274a000e96d2f2b13e327893c32
renatogsouza <renatomacedo1995@gmail.com>
2021-05-14 20:07:57 -0300

    requisito 4 iniciado

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ function App() {
     <div className="App">
       <div>
         <label htmlFor="search" data-testid="home-initial-message">
-          <input type="text" name="search" value="search-text" />
+          <input type="text" name="search" />
 
           Digite algum termo de pesquisa ou escolha uma categoria.
+        </label>
+        <label htmlFor="category1">
+          <input type="radio" name="category1" id="category1" />
+          Categoria 1
         </label>
       </div>
       <BrowserRouter>
