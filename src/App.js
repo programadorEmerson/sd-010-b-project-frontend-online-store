0bd165fa338ad93e4b9396fde13eb71b46ff2708
Daniel <darkdanielr@gmail.com>
2021-05-14 16:23:52 -0300

    requisito1 2.0

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { getCategories } from './services/api';
 
 function App() {
   return (
     <div className="App">
-      <h1>Categories</h1>
-      {getCategories.map(({ id, name }) => (
-        <div id={ id } key={ id }>
-          <p>{name}</p>
-        </div>
-      ))}
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <p>Edit src/App.js and save to reload.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
-// bodega
