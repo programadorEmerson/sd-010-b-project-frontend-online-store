465e0a0fb2b6a678860d8278ead7a60492e796dd
Fabio Marturano <fabiocmjor@gmail.com>
2021-05-14 20:58:02 -0300

    Req 5

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,41 +1,26 @@
-import { findByTestId } from '@testing-library/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+
+import { getProductsFromCategoryAndQuery } from '../services/api';
 import CategoryList from './CategoryList';
 
 class Home extends React.Component {
   constructor() {
     super();
     this.state = {
-      filtered: false,
-      listProduct: [],
+      filtered: [],
       search: '',
     };
-    this.getResult = this.getResult.bind(this);
-    this.requestApi = this.requestApi.bind(this);
-    this.requestApi2 = this.requestApi2.bind(this);
   }
 
-  componentDidMount() {
-    this.requestApi();
-    // this.requestApi2();
+  // componentDidMount() {
+  // }
+
+  getResult = ({ target: { value } }) => {
+    this.setState({ search: value });
   }
 
-  getResult(event) {
-    this.setState({
-      search: event.target.value,
-    });
-  }
-
-  async requestApi() {
-    const result = await getCategories();
-    this.setState({
-      listProduct: result,
-    });
-  }
-
-  async requestApi2() {
+  requestApi2 = async () => {
     const { search } = this.state;
     const result1 = await getProductsFromCategoryAndQuery('', search);
     this.setState({
@@ -45,8 +30,8 @@ class Home extends React.Component {
 
   render() {
     const { filtered } = this.state;
+    console.log(filtered);
     const { available_filters } = filtered;
-    console.log(available_filters);
     return (
       <div>
         <div>
@@ -70,10 +55,14 @@ class Home extends React.Component {
           </ol>
           <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
         </div>
-        {!filtered || filtered.available_filters.length === 0 ? 'Nenhum produto a ser encontrado'
-          : (filtered.results.map(
-            (product) => (<li key={ product.id }>{product.title}</li>),
-          ))}
+        {filtered.length !== 0
+        && (<div id="products">
+          {!available_filters || available_filters.length === 0
+            ? 'Nenhum produto a ser encontrado'
+            : (filtered.results.map(
+              (product) => (<li data-testid="product" key={ product.id }>{product.title}</li>),
+            ))}
+        </div>) }
       </div>
     );
   }
