721be245550d30775cc5d0cf9359c0972001b825
esionascimento <74088591+esionascimento@users.noreply.github.com>
2021-05-15 10:53:48 -0400

    Update src/pages/Search.js comment
    Co-authored-by: Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as api from '../services/api';
-/* import PropTypes from 'prop-types'; */
 import ListCategories from './Listcategories';
 import CartImage from '../components/CartImage';
 import CartListProduct from '../components/CartListProduct';
