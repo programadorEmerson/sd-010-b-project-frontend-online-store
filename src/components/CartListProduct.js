c6e42d71a332254ffc26eb8c33ebeb3e525d836b
esionascimento <74088591+esionascimento@users.noreply.github.com>
2021-05-15 10:48:36 -0400

    Update src/components/CartListProduct.js
    Co-authored-by: Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>

--- src/components/CartListProduct.js
+++ src/components/CartListProduct.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 export default class CartListProduct extends Component {
   render() {
     const { product: { title, thumbnail, price } } = this.props;
-    /* const { title, img, prices } = product; */
     return (
       <div data-testid="product">
         <h3>{ title }</h3>
