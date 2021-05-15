445295e0085f54bd5f52b47619dd55becc15322b
Flavio Andrade <foa12381@gmail.com>
2021-05-15 15:01:10 -0300

    implementa redudancia do requesito 9

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import ButtonToCart from '../components/ButtonToCart';
 
 class ProductDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cart: [],
+    };
+    this.addProductIntoCart = this.addProductIntoCart.bind(this);
+  }
+
+  addProductIntoCart(item) {
+    const { cart } = this.state;
+    this.setState({ cart: [...cart, item] });
+  }
+
   render() {
     const { location: { state: { item } } } = this.props;
     const { title } = item;
+    const { cart } = this.state;
     return (
-      <p data-testid="product-detail-name">{ title }</p>
+      <div>
+        <Link
+          to={ { pathname: '/cart', state: { cart } } }
+          data-testid="shopping-cart-button"
+        >
+          <ButtonToCart />
+        </Link>
+        <p data-testid="product-detail-name">{ title }</p>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ () => this.addProductIntoCart(item) }
+        >
+          Adicionar
+
+        </button>
+      </div>
     );
   }
 }
