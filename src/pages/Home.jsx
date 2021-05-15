63da03d9b193f76dfbfc8990d48e021c237cdfdb
DennisMarcelo <dennismarcelo15@gmail.com>
2021-05-15 18:51:38 -0300

    requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import ListCategories from '../components/Home/Listcategories';
 import Search from '../components/Home/Search';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      query: '',
+      category: '',
+    };
+  }
+
+  handlerChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
     return (
-      <Search />
+      <>
+        <Search />
+        <ListCategories handlerChange={ this.handlerChange } />
+      </>
     );
   }
 }
