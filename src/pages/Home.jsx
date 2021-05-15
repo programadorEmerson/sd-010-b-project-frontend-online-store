be24cbfb14c54dee51441f1a79768251e6982e89
Carlos Vieira <appencigr@gmail.com>
2021-05-15 17:36:59 -0300

    Grupo-11-Requisito5-Buscando Modificações

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ class Home extends React.Component {
         <section className="result-page">
           <Input />
           <p className="text-search" data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
+            Digite algum termo de Pesquisa ou escolha uma categoria.
           </p>
           <section className="products-list">
             <ProductsList />
