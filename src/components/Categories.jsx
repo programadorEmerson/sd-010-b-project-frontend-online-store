25fccee23c013ffe47acdad7fa67377aba1f4871
Dieghonm <dieghonm@gmail.com>
2021-05-16 01:35:45 -0300

    requisito 4, tentativa 1

---
+++ src/components/Categories.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import * as api from '../services/api';
+import Category from './Category'
+
+class Categories extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: []
+    }
+  }
+
+  componentDidMount(){
+    this.apiCategories()
+  }
+
+  apiCategories() {
+    api.getCategories().then((api) => this.setState({
+      categories : api
+    }));
+  }
+
+  render() {
+    const { categories } = this.state
+    return (
+      <div>
+        {console.log(categories)}
+        {categories.map((category) => <Category key={ category.id }category={ category } /> )}
+      </div>
+    );
+  }
+}
+
+export default Categories;
---
+++ src/components/Category.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Category extends React.Component {
+  render() {
+    const { category  } = this.props;
+    return(
+      <p data-testid='category'>{`${category.name}`}</p>
+    );
+  }
+}
+
+export default Category;
