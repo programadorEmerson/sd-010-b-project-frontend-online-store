9580ab33d8ffa17529b50fa607a5d9c316dda974
riquee <strikedv@hotmail.com>
2021-05-16 14:46:23 -0300

    correcting lints(services)

--- src/services/createForms/generator.js
+++ src/services/createForms/generator.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Input from './elements/Input';
-import Select from './elements/Select';
 
 class CreateElement extends Component {
   render() {
     const { forms } = this.props;
-    return forms.map((e, idx) => {
-      const props = { key: `formExp - ${idx}`, ...e };
-      return e.options ? <Select { ...props } /> : <Input { ...props } />;
-    });
+    return forms.map((e, idx) => <Input key={ `formExp - ${idx}` } { ...e } />);
   }
 }
 
