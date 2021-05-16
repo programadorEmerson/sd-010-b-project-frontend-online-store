ec75ed4a46b50ab5b30ad90cc7a89b78caeb9dfe
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-05-16 08:27:37 -0300

    Inserido a função para pesquisar e Finalizado o requisito 5

--- src/components/listProducts.jsx
+++ src/components/listProducts.jsx
@@ -11,6 +11,8 @@ class ListProducts extends React.Component {
 
     this.getProcucts = this.getProcucts.bind(this);
     this.handleOnchange = this.handleOnchange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
 
   componentDidMount() {
@@ -23,6 +25,23 @@ class ListProducts extends React.Component {
     this.setState({
       [name]: value,
     });
+    if (!value) {
+      this.getProcucts();
+    }
+  }
+
+  handleClick() {
+    const { query, products } = this.state;
+    this.setState({ products: products.filter(
+      (product) => product.title.includes(query),
+    ),
+    });
+  }
+
+  handleEnter(event) {
+    if (event.key === 'Enter') {
+      this.handleClick();
+    }
   }
 
   async getProcucts() {
@@ -34,8 +53,19 @@ class ListProducts extends React.Component {
     const { products } = this.state;
     return (
       <section>
-        <input name="query" ata-testid="query-input" onChange={ this.handleOnchange } />
-        <button type="button" data-testid="query-button">teste</button>
+        <input
+          name="query"
+          data-testid="query-input"
+          onKeyDown={ this.handleEnter }
+          onChange={ this.handleOnchange }
+        />
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
         <div className="product-list">
           { products.map((element) => (
             <div className="product-card" data-testid="product" key={ element.id }>
