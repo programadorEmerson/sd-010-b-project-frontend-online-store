2972a77450501c885b92556c6e87f91f4c4f8421
Daniel <darkdanielr@gmail.com>
2021-05-17 19:38:46 -0300

    5feito

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as api from '../services/api';
 
 class CategoryList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: '',
+      searchText: [],
+    };
+  }
+
+  search = () => {
+    const { search } = this.state;
+    api.getProductsFromCategoryAndQuery(undefined, search).then(({ results }) => {
+      this.setState({
+        searchText: results,
+      });
+    });
+  }
+
   render() {
     const { categories } = this.props;
     return (
       <aside>
         <ol>
           { categories.map(({ id, name }) => (
-            <li data-testid="category" key={ id }>{ name }</li>
+            <li data-testid="category" key={ id }><button type="button">{ name }</button></li>
           )) }
         </ol>
       </aside>
--- src/components/SearchList.jsx
+++ src/components/SearchList.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 class SearchList extends React.Component {
   render() {
+    const { item } = this.props;
+    const { title, thumbnail, price } = item;
     return (
-      <div>SearchList</div>
+      <li data-testid="product">{title} <img src={thumbnail} /> {price}</li>
     );
   }
 }
