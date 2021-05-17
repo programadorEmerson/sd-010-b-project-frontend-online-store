60a8035637ad88beaead3b98c6cfe7a85365746c
Cristian <cristian.bugs@gmail.com>
2021-05-17 16:26:51 -0300

    requirement 08

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -3,11 +3,19 @@ import PropTypes from 'prop-types';
 
 class ListProducts extends Component {
   renderProduct({ title, price, thumbnail }, index) {
+    const { setCart } = this.props;
     return (
       <div data-testid="product" key={ `${index} - ${title}` }>
         <p>{title}</p>
         <img src={ thumbnail } alt={ title } />
         <p>{price}</p>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          onClick={ setCart({ title, price, thumbnail, quant: 0 }) }
+        >
+          ADICIONAR AO CARRINHO
+        </button>
       </div>
     );
   }
@@ -21,6 +29,7 @@ class ListProducts extends Component {
 
 ListProducts.propTypes = {
   products: PropTypes.arrayOf(PropTypes.object).isRequired,
+  setCart: PropTypes.func.isRequired,
 };
 
 export default ListProducts;
