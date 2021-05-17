1e5d91ad22180d93883c5fb0c235272b9b3e1022
Caio <thehaseol@gmail.com>
2021-05-17 14:16:48 -0300

    Comitando alteraçoes

--- src/components/productsByTerms.jsx
+++ src/components/productsByTerms.jsx
@@ -1,17 +1,18 @@
-import * as api  from '../services/api';
 import React from 'react';
+import * as api from '../services/api';
 
 class ProductsByTerms extends React.Component {
-  render () {
+  render() {
     const { product: { title, price, thumbnail } } = this.props;
     return (
       <div data-testid="product">
         <p>{title}</p>
         <p>{price}</p>
-        <img src={thumbnail} alt={title}/>
+        <img src={ thumbnail } alt={ title } />
       </div>
-    )
+    );
   }
 }
 
 export default ProductsByTerms;
+j
