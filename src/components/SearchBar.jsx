c1d178e95441ceb9234d01670b110658026fdb36
Malbathan <malbathanlima@gmail.com>
2021-05-17 12:37:18 -0300

    searchbar css

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ class SeachBar extends React.Component {
     return (
       <section>
         <label htmlFor="input">
-          <input type="text" id="input" />
+          <input type="text" id="search-bar-input" />
         </label>
         <Button />
         <h1
