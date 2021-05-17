cff4be2366a21ada1748c34323b04d6761b1b1f6
riquee <strikedv@hotmail.com>
2021-05-17 15:14:59 -0300

    add render list products

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import ProductCart from '../components/ProductCar';
 
 class ShoppingCart extends Component {
-  render() {
+  constructor() {
+    super();
+    this.renderList = this.renderList.bind(this);
+    this.renderIsEmpty = this.renderIsEmpty.bind(this);
+  }
+
+  renderList() {
     const { cart } = this.props;
-    const emptyCart = (
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-    );
-    return <div>{cart.length < 1 ? emptyCart : ' '}</div>;
+    return cart.map((product, index) => (
+      <ProductCart key={ `${product.title} - ${index}` } product={ product } />
+    ));
+  }
+
+  renderIsEmpty() {
+    return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
+  }
+
+  render() {
+    return <div>{cart.length < 1 ? this.renderIsEmpty() : this.renderList()}</div>;
   }
 }
 
