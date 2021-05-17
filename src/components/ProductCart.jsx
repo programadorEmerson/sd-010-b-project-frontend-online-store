ec1487cac147a53a53010f5f8992237b636a6c20
riquee <strikedv@hotmail.com>
2021-05-17 18:51:11 -0300

    tenth task

--- src/components/ProductCart.jsx
+++ src/components/ProductCart.jsx
@@ -4,11 +4,21 @@ import PropTypes from 'prop-types';
 class ProductCart extends Component {
   render() {
     const {
-      product: { title, quant },
+      product: { title, quant, id },
+      increaseProduct,
     } = this.props;
+    console.log(increaseProduct);
     return (
       <div>
         <p data-testid="shopping-cart-product-name">{title}</p>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ () => increaseProduct(id) }
+        >
+          +
+        </button>
+        <button type="button" data-testid="product-decrease-quantity">+</button>
         <p data-testid="shopping-cart-product-quantity">{quant}</p>
       </div>
     );
@@ -19,7 +29,9 @@ ProductCart.propTypes = {
   product: PropTypes.shape({
     title: PropTypes.string,
     quant: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
+  increaseProduct: PropTypes.func.isRequired,
 };
 
 export default ProductCart;
