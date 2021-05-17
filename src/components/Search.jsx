0bd43471c7e8264a5c631b3cf17f99a5ee2d141b
denis-rossati <denis.rossati@hotmail.com>
2021-05-17 16:22:18 -0300

    req 5 OK

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Categories from './Categories';
 
 class Search extends Component {
   render() {
+    const { onChange, onClick } = this.props;
     return (
       <section>
         <input
           type="text"
+          onChange={ onChange }
+          data-testid="query-input"
         />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
         <button
           type="button"
+          onClick={ onClick }
+          data-testid="query-button"
+        >
+          Pesquisar
+        </button>
+        <button
+          type="button"
         >
           <Link
             to="/checkout"
             data-testid="shopping-cart-button"
-          />
+          >
+            Carrinho
+          </Link>
         </button>
         <Categories />
       </section>
     );
   }
 }
+
+Search.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Search;
