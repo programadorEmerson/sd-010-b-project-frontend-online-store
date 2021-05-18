15b246fdcfe85e1e047b0fbc52f4aa980076bea4
lucasportella <lucasportellaagu@gmail.com>
2021-05-18 16:31:53 -0300

    getItems not working

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import ItemProductCart from './ItemProductCart';
 
 class Cart extends React.Component {
   render() {
-    return (<p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>);
+    const cartItems = JSON.parse(localStorage.getItem('cartState'));
+    if (!cartItems) {
+      return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
+    }
+    return (
+      <div>
+        {cartItems.map((cartItem, index) => (
+          <ItemProductCart cartItem={ cartItem } key={ index + 1 } />
+        ))}
+      </div>
+    );
   }
 }
 
--- src/components/ItemProductCart.js
+++ src/components/ItemProductCart.js
@@ -3,22 +3,16 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ItemProductCart extends React.Component {
-
   render() {
-    const {
-      product: { title, thumbnail, price, id },
-    } = this.props;
+    const { cartItem } = this.props;
+    console.log(cartItem)
     return (
       <section data-testid="product">
         <fieldset>
-          <p>{title}</p>
-          <img src={ thumbnail } alt={ title } />
-          <p>{`R$${price}`}</p>
-          <Link data-testid="product-detail-link" to={ `/detail/${id}` }>
-            {' '}
-            Detalhes
-          </Link>
-          <button type="button" onClick={ this.addToCart }>Adicionar ao Carrinho</button>
+          <p>{cartItem.title}</p>
+          {/* <p>{ id }</p>
+          <img src={thumbnail} alt={title} />
+          <p>{`R$${price}`}</p> */}
         </fieldset>
       </section>
     );
