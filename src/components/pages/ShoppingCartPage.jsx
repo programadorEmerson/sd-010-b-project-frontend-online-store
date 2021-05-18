0ee949008336b144c87ce4bfb37dc93db5fc9084
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-05-18 17:37:04 -0300

    adicionando proptypes

--- src/components/pages/ShoppingCartPage.jsx
+++ src/components/pages/ShoppingCartPage.jsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ShoppingCartPage extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: props.itemShoppingCart,
+    };
+    this.getAllProducts = this.getAllProducts.bind(this);
+  }
+
+  shouldComponentUpdate() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
     const { itemShoppingCart } = this.props;
+    this.setState(({ item }) => ({
+      products: [...item, itemShoppingCart],
+    }));
+  }
+
+  render() {
+    const { products } = this.state;
     return (
       <>
         <h1>
           Shopping Cart Page
         </h1>
-
         <section>
-          {(itemShoppingCart.length === 0)
+          {(products.length === 0)
             ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-            : itemShoppingCart.map((item) => (
+            : products.map((item) => (
               <div key={ item.id }>
                 <h6 data-testid="shopping-cart-product-name">{ item.title }</h6>
                 <img src={ item.thumbnail } alt={ item.site_id } width="30px" />
@@ -20,7 +40,17 @@ export default class ShoppingCartPage extends Component {
               </div>
             ))}
         </section>
+        <section>
+          <p data-testid="shopping-cart-product-quantity">
+            Quantidade de items no carrinho:
+            {products.length}
+          </p>
+        </section>
       </>
     );
   }
 }
+
+ShoppingCartPage.propTypes = {
+  itemShoppingCart: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
