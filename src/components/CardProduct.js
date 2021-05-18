b594d491fbc90c7ff8f487c71c7d4bc6979a436d
lucasportella <lucasportellaagu@gmail.com>
2021-05-18 11:49:56 -0300

    start requirement 8

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -4,19 +4,21 @@ import PropTypes from 'prop-types';
 
 class CardProduct extends React.Component {
   render() {
-    const { product: { title, thumbnail, price, id } } = this.props;
+    const {
+      product: { title, thumbnail, price, id },
+    } = this.props;
     return (
       <section data-testid="product">
-        <p>{ title }</p>
-        <img src={ thumbnail } alt={ title } />
-        <p>{ `R$${price}` }</p>
-        <Link
-          data-testid="product-detail-link"
-          to={ `/detail/${id}` }
-        >
-          {' '}
-          Detalhes
-        </Link>
+        <fieldset>
+          <p>{title}</p>
+          <img src={ thumbnail } alt={ title } />
+          <p>{`R$${price}`}</p>
+          <Link data-testid="product-detail-link" to={ `/detail/${id}` }>
+            {' '}
+            Detalhes
+          </Link>
+          <button type="button">Adicionar ao Carrinho</button>
+        </fieldset>
       </section>
     );
   }
