4344a48f61f65823b64745937fd6cb1e771ec007
EduardoSantosF <eduardo.debest81@gmail.com>
2021-05-18 19:19:12 -0300

    empty details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,57 +1,57 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import getProductById from '../services/api2';
-import * as api from '../services/api';
-import Loading from '../components/Loading';
+// import PropTypes from 'prop-types';
+// import getProductById from '../services/api2';
+// import * as api from '../services/api';
+// import Loading from '../components/Loading';
 
-//teste
+// teste
 
 class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.fetchProduct = this.fetchProduct.bind(this);
-    this.state = {
-      productDetails: {},
-      fetched: false,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.fetchProduct = this.fetchProduct.bind(this);
+  //   this.state = {
+  //     productDetails: {},
+  //     fetched: false,
+  //   };
+  // }
 
-  componentDidMount() {
-    this.fetchProduct();
-  }
+  // componentDidMount() {
+  //   this.fetchProduct();
+  // }
 
-  async fetchProduct() {
-    const { match: { params: { id } } } = this.props;
-    const productDetails = await getProductById(id);
-    this.setState({ productDetails, fetched: true });
-    await api.getProductsFromCategoryAndQuery();
-  }
+  // async fetchProduct() {
+  //   const { match: { params: { id } } } = this.props;
+  //   const productDetails = await getProductById(id);
+  //   this.setState({ productDetails, fetched: true });
+  //   await api.getProductsFromCategoryAndQuery();
+  // }
 
   render() {
-    const { productDetails, fetched } = this.state;
-    if (!fetched) return <Loading />;
-    const { title, thumbnail, attributes } = productDetails;
+    // const { productDetails, fetched } = this.state;
+    // if (!fetched) return <Loading />;
+    // const { title, thumbnail, attributes } = productDetails;
     return (
       <div>
-        <h3 data-testid="product-detail-name">{title}</h3>
+        {/* <h3 data-testid="product-detail-name">{title}</h3>
         <img src={ thumbnail } alt={ title } />
         <ul>
           {attributes?.map((attribute) => (
             <li key={ attribute.id }>
               { `${attribute.name}: ${attribute.value_name}` }
             </li>))}
-        </ul>
+        </ul> */}
       </div>
     );
   }
 }
 
-ProductDetails.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      id: PropTypes.string,
-    }),
-  }).isRequired,
-};
+// ProductDetails.propTypes = {
+//   match: PropTypes.shape({
+//     params: PropTypes.shape({
+//       id: PropTypes.string,
+//     }),
+//   }).isRequired,
+// };
 
 export default ProductDetails;
