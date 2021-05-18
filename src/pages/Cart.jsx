f469058af719c19fe7b65ce4f37fc866abc0f2b7
Italo Matos <italomatos764@gmail.com>
2021-05-18 15:20:57 -0400

    Corrigindo erro de lint

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -46,7 +46,7 @@ class Cart extends React.Component {
             <BtnDel product={ product } handleReload={ this.handleReload } />
           </div>
         ))}
-        {/* <p>{this.handleQuantity()} </p> */}
+        {/* <p>{this.hand*leQuantity()} </p> */}
         {/* <p>
           {this.handleTotalCart()}
         </p>  */}
