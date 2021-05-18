26ec9bf0bdcb0128cddd006805b03dfa8339f8c7
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-18 19:28:11 -0300

    tetando a 9

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -8,14 +8,29 @@ class Detail extends React.Component {
     this.state = {
       produto: {},
       loading: true,
+      cartState: [],
     };
 
     this.recuperarProduto = this.recuperarProduto.bind(this);
     this.getProductsFromId = this.getProductsFromId.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
+    this.addToCart = this.addToCart.bind(this);
   }
 
   componentDidMount() {
     this.recuperarProduto();
+  }
+
+  handleAddToCart(cardProps) {
+    this.setState((oldState) => ({
+      cartState: [
+        ...oldState.cartState, { cardProps },
+      ],
+    }),
+    () => {
+      const { cartState } = this.state;
+      localStorage.setItem('cartState', JSON.stringify(cartState));
+    });
   }
 
   async getProductsFromId(productId) {
@@ -23,6 +38,17 @@ class Detail extends React.Component {
     return fetch(URL)
       .then((result) => result.json())
       .catch((error) => { console.log(`Erro na requisição: ${error}`); });
+  }
+
+  addToCart() {
+    const { produto: { title, thumbnail, price, id } } = this.state;
+    const cartItem = {
+      title,
+      thumbnail,
+      price,
+      id,
+    };
+    this.handleAddToCart(cartItem);
   }
 
   recuperarProduto() {
@@ -51,6 +77,13 @@ class Detail extends React.Component {
                   ))
                 }
               </ol>
+              <button
+                type="button"
+                data-testid="product-detail-add-to-cart"
+                onClick={ this.addToCart }
+              >
+                Adicionar
+              </button>
             </div>
           )}
       </div>
