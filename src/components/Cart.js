2fb1bd5c6abeb8e9b0c736e91978de6054aaba87
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-05-18 18:33:44 -0300

    tudo com sucesso

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,10 +4,10 @@ import ItemProductCart from './ItemProductCart';
 class Cart extends React.Component {
   render() {
     const cartItems = JSON.parse(localStorage.getItem('cartState'));
-    const total = cartItems.length;
     if (!cartItems) {
       return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
     }
+    const total = cartItems.length;
     return (
       <div>
         <p data-testid="shopping-cart-product-quantity">{ total }</p>
