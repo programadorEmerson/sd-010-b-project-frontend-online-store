2ff3fc613a19a61e770242a7aa8fbec33a42a026
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-18 17:47:57 -0300

    Começando requisito 10 adicionando branch do requisito 7

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import ProductCard from '../components/ProductCart';
+
 class Checkout extends React.Component {
   render() {
-    if (!localStorage.getItem('shoppingCart')) {
+    const cartList = localStorage.getItem('shoppintCart');
+    if (!cartList) {
       return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio </p>;
     }
+    return (
+      <div>
+        {cartList.map((item) => (
+          <ProductCard key={ item.id } product={ item } />
+        ))}
+      </div>
+    );
   }
 }
 
