99cb99465fccd2553e9f91abda4d99e1b3c415ed
Junior030 <juniorhenrique030@gmail.com>
2021-05-18 20:56:03 -0300

    Requesito 11 completo version 1.0

--- src/components/pages/ItemProduct.jsx
+++ src/components/pages/ItemProduct.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import EvaluationArea from '../EvaluationArea';
 
 export default class ItemProduct extends Component {
   constructor(props) {
     super(props);
     this.state = {
       item: {},
+      email: '',
+      rating: 0,
+      menssage: '',
     };
     this.fetchItem = this.fetchItem.bind(this);
+    this.evaluationData = this.evaluationData.bind(this);
   }
 
   componentDidMount() {
@@ -20,22 +25,110 @@ export default class ItemProduct extends Component {
     this.setState({ item });
   }
 
+  evaluationData({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
-    const { item } = this.state;
+    const { item, email, menssage, rating } = this.state;
     return (
-      <>
-        <div>
-          Produto:
-          {' '}
-          <strong data-testid="product-detail-name">{item.title}</strong>
-        </div>
-        <div>
-          Preço:
-          {' '}
-          <strong>{item.price}</strong>
-        </div>
-        <img src={ item.thumbnail } alt="imagem do produto" />
-      </>
+      <main>
+        <section>
+          <div>
+            Produto:
+            {' '}
+            <strong data-testid="product-detail-name">{item.title}</strong>
+          </div>
+          <div>
+            Preço:
+            {' '}
+            <strong>{item.price}</strong>
+          </div>
+          <img src={ item.thumbnail } alt="imagem do produto" />
+        </section>
+        <form>
+          <label htmlFor="email">
+            <input
+              type="text"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={ this.evaluationData }
+            />
+          </label>
+          <div className="estrelas">
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-empty"
+              name="rating"
+              value=""
+              checked
+              onChange={ this.evaluationData }
+            />
+            <i className="fa" />
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-1"
+              name="rating"
+              value="1"
+              onChange={ this.evaluationData }
+            />
+            <i className="fa" />
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-2"
+              name="rating"
+              value="2"
+              onChange={ this.evaluationData }
+            />
+            <i className="fa" />
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-3"
+              name="rating"
+              value="3"
+              onChange={ this.evaluationData }
+            />
+            <i className="fa" value="4" />
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-4"
+              name="rating"
+              value="4"
+              onChange={ this.evaluationData }
+            />
+            <i className="fa" />
+            <input
+              type="radio"
+              className="inputRadio"
+              id="cm_star-5"
+              name="rating"
+              value="5"
+              onChange={ this.evaluationData }
+            />
+          </div>
+          <br />
+          <label htmlFor="textArea">
+            <textarea
+              id="textArea"
+              name="menssage"
+              placeholder="Mensagem (opcional)"
+              data-testid="product-detail-evaluation"
+              onChange={ this.evaluationData }
+            />
+          </label>
+          <br />
+          <button type="button">Avaliar</button>
+        </form>
+        <EvaluationArea dados={ { email, menssage, rating } } />
+      </main>
     );
   }
 }
