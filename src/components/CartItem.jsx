7451423ab41adb471da81c5d04d4f30bff72f5ad
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-18 14:27:05 -0300

    o estado cart, as funções do cart e o consumo do estado cart foi refatorado, e adicionado o controle de quantidade e preço no cart

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CartItem extends React.Component {
+  render() {
+    const { name, handleCartBtnEvent, qty, id, price } = this.props;
+    return (
+      <div>
+        <p data-testid="shopping-cart-product-quantity">{qty}</p>
+        <p data-testid="shopping-cart-product-name">{name}</p>
+        <button
+          data-testid="product-increase-quantity"
+          name="plus"
+          type="button"
+          onClick={ (e) => handleCartBtnEvent(e, id) }
+        >
+          Mais
+        </button>
+        <button
+          data-testid="product-decrease-quantity"
+          name="subtract"
+          type="button"
+          onClick={ (e) => handleCartBtnEvent(e, id) }
+        >
+          Menos
+
+        </button>
+        <button
+          name="remove"
+          type="button"
+          onClick={ (e) => handleCartBtnEvent(e, id) }
+        >
+          X
+
+        </button>
+        <p>
+          Total: R$
+          {' '}
+          {price * qty }
+        </p>
+      </div>
+    );
+  }
+}
+
+CartItem.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  qty: PropTypes.string.isRequired,
+  handleCartBtnEvent: PropTypes.func.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+export default CartItem;
