7bf8355f40eac3be0d77698a917292c370603e7e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-05-19 10:50:57 -0300

    Import da função para adicionare no local storage

--- src/components/Home/CartListProduct.jsx
+++ src/components/Home/CartListProduct.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { addLocalStorage } from '../../services/dataLocalStorage';
 
 export default class CartListProduct extends Component {
   render() {
