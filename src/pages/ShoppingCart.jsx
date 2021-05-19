43c5f12ca684444de5b33fefbd9944c97711873e
Lucas Ribeiro <lucasrb04@gmail.com>
2021-05-19 08:11:31 -0300

    Botao de adicionar e diminuir a quatidade de produtos no carrinho

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -3,6 +3,25 @@ import React from 'react';
 import ProductCard from '../components/ProductCard';
 
 class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: props.quantity,
+    };
+  }
+
+  handleClick = (operator) => {
+    const { quantity } = this.state;
+    if (operator === '+') {
+      this.setState({
+        quantity: quantity + 1,
+      });
+    }
+    this.setState({
+      quantity: quantity - 1,
+    });
+  }
+
   render() {
     const cartList = localStorage.getItem('shoppintCart');
     if (!cartList) {
@@ -11,7 +30,26 @@ class ShoppingCart extends React.Component {
     return (
       <div>
         {cartList.map((item) => (
-          <ProductCard key={ item.id } product={ item } />
+          <div key={ item.id }>
+            <ProductCard key={ item.id } product={ item } />
+            <button
+              type="button"
+              key={ item.id }
+              onClick={ this.handleClick('+') }
+              data-testid="product-increase-quantity"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              key={ item.id }
+              onClick={ this.handleClick('-') }
+              data-testid="product-decrease-quantity"
+            >
+              +
+            </button>
+          </div>
+
         ))}
       </div>
     );
