dd51258067d73f139d587168dcef79b53d3f6c44
Gabriel Dias <gabriel.menezes.dias@hotmail.com>
2021-05-19 16:20:19 -0300

    fix bug

--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CartList from '../Components/CartList';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+
+    this.cartList = this.cartList.bind(this);
+    this.emptyCart = this.emptyCart.bind(this);
+  }
+
+  emptyCart() {
+    return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
+  }
+
+  cartList() {
+    const { cart, totalValue } = this.props;
+    return cart.map((product, curr) => (
+      <CartList
+        totalValue={ totalValue }
+        key={ `${product.title} - ${curr}` }
+        product={ product }
+      />
+    ));
+  }
+
   render() {
-    return (
-      <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-      </div>
-    );
+    const { cart } = this.props;
+    return <div>{ cart.length > 0 ? this.cartList() : this.emptyCart() }</div>;
   }
 }
+
+ShoppingCart.propTypes = {
+  cart: PropTypes.arrayOf,
+  totalValue: PropTypes.func,
+}.isRequired;
 
 export default ShoppingCart;
