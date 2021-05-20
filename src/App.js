3191f5b78488a3ea1b262731102d8ed5434648b5
Dieghonm <dieghonm@gmail.com>
2021-05-20 19:36:39 -0300

    Requisito 7 mergeado 3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-<<<<<<< HEAD
 import Cart from './components/Cart';
-=======
->>>>>>> 4dd7746b962c93d1422de49827e84258972e7437
 import ProductDetails from './components/ProductDetails';
 
 function App() {
@@ -13,11 +10,8 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
-<<<<<<< HEAD
         <Route path="/home/:id" component={ Home } />
         <Route exact path="/cart" component={ Cart } />
-=======
->>>>>>> 4dd7746b962c93d1422de49827e84258972e7437
         <Route
           exact
           path="/products/:id"
