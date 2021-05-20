e2a7d7aff31aef126ed3f74c948c30299bcba2df
Lara Capila <laracapilak@gmail.com>
2021-05-20 16:10:56 -0300

    tentando consertar as coisas

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Cart extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
     const { shoppingCartProduct } = this.props;
+    this.state = {
+      teste: shoppingCartProduct,
+    };
+  }
+
+  render() {
+    console.log(this.state.teste);
     return (
       <section>
-        {shoppingCartProduct.length === 0 ? (
+        {/* {shoppingCartProduct === undefined ? (
           <span data-testid="shopping-cart-empty-message">
             Seu carrinho está vazio
           </span>
-        ) : (
+        ) : ( */}
           <div>
-            {shoppingCartProduct.map((product) => (
+            {this.state.teste.map((product) => (
               <div key={ product.id }>
                 <div
                   data-testid="shopping-cart-product-name"
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Input from '../components/Input';
 import Categories from '../components/Categories';
 import ProductList from '../components/ProductList';
+import Cart from './Cart';
 
 class MainPage extends React.Component {
   constructor() {
@@ -9,9 +10,12 @@ class MainPage extends React.Component {
     this.state = {
       products: [],
       queryTerm: '',
+      // shoppingCartProduct: localStorage.getItem('cart') || [],
+      shoppingCartProduct: [],
     };
 
     this.handleQuery = this.handleQuery.bind(this);
+    this.addItemToCart = this.addItemToCart.bind(this);
   }
 
   handleQuery(products, query) {
@@ -21,16 +25,38 @@ class MainPage extends React.Component {
     });
   }
 
+  addItemToCart(id, title, price) {
+    const { shoppingCartProduct } = this.state;
+    console.log('ola');
+
+    if (shoppingCartProduct.find((product) => product.id === id)) {
+      const index = shoppingCartProduct.findIndex((product) => product.id === id);
+      shoppingCartProduct[index].quantity += 1;
+    } else {
+      const newCartProduct = { id, title, price, quantity: 1 };
+      shoppingCartProduct.push(newCartProduct);
+    }
+    this.setState({ shoppingCartProduct });
+    // localStorage.setItem('cart', JSON.stringify(shoppingCartProduct));
+  }
+
   render() {
-    const { products, queryTerm } = this.state;
+    const { products, queryTerm, shoppingCartProduct } = this.state;
     return (
       <main>
+        <Input
+          handleQuery={ this.handleQuery }
+          shoppingCartProduct={ shoppingCartProduct }
+        />
+        <Cart
+          shoppingCartProduct={ shoppingCartProduct }
+          addItemToCart={ this.addItemToCart }
+        />
         <Categories
           handleQuery={ this.handleQuery }
           query={ queryTerm }
         />
-        <Input handleQuery={ this.handleQuery } />
-        <ProductList products={ products } />
+        <ProductList products={ products } addItemToCart={ this.addItemToCart } />
       </main>
     );
   }
